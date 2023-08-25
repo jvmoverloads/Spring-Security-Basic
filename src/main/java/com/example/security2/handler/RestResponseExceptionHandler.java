89305689b5d4ed65e015259d72df61ad2731e0dd
채희승(Hee seung Chae)(chs4413) <chs4413@ncits.co.kr>
2023-08-25 17:44:12 +0900

    refactor - @RestControllerAdvice(@ControllerAdvice + @ResponseBody) 리팩토링

--- src/main/java/com/example/security2/handler/RestResponseExceptionHandler.java
+++ src/main/java/com/example/security2/handler/RestResponseExceptionHandler.java
@@ -3,11 +3,7 @@ package com.example.security2.handler;
 import com.example.security2.dto.ErrorDto;
 import com.example.security2.exception.DuplicateMemberException;
 import com.example.security2.exception.NotFoundMemberException;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -16,19 +12,17 @@ import java.nio.file.AccessDeniedException;
 import static org.springframework.http.HttpStatus.CONFLICT;
 import static org.springframework.http.HttpStatus.FORBIDDEN;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class RestResponseExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ResponseStatus(CONFLICT)
     @ExceptionHandler(value = { DuplicateMemberException.class })
-    @ResponseBody
     protected ErrorDto conflict(RuntimeException ex, WebRequest request) {
         return new ErrorDto(CONFLICT.value(), ex.getMessage());
     }
 
     @ResponseStatus(FORBIDDEN)
     @ExceptionHandler(value = { NotFoundMemberException.class, AccessDeniedException.class })
-    @ResponseBody
     protected ErrorDto forbidden(RuntimeException ex, WebRequest request) {
         return new ErrorDto(FORBIDDEN.value(), ex.getMessage());
     }
