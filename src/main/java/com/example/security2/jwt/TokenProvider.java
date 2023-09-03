b95ba826a282e5497dcf0cc70c3fccf4217faf83
채희승 <chs4413@gmail.com>
2023-09-03 20:25:08 +0900

    fix - 토큰 유효성 검증 메서드 > 토큰 Claim 리턴 함수로 변경

--- src/main/java/com/example/security2/jwt/TokenProvider.java
+++ src/main/java/com/example/security2/jwt/TokenProvider.java
@@ -7,7 +7,6 @@ import io.jsonwebtoken.security.SecurityException;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -27,27 +26,18 @@ import java.util.stream.Collectors;
  */
 @Component
 @Slf4j
-public class TokenProvider implements InitializingBean {
+public class TokenProvider {
 
     private static final Logger logger = LoggerFactory.getLogger(TokenProvider.class);
     private static final String AUTHORITIES_KEY = "secret";
-    private final String secret;
     private final long tokenValidityInMilliseconds;
-
-    private Key key;
+    private final Key key;
 
     public TokenProvider(
             @Value("${jwt.secret}") String secret,
             @Value("${jwt.token-validity-in-seconds}") long tokenValidityInMilliseconds) {
-        this.secret = secret;
         this.tokenValidityInMilliseconds = tokenValidityInMilliseconds * 1000;
-    }
-
-    // 빈이 생성되고 주입을 받은 후에 secret 값을 Base64 Decode해서 Key 변수에 할당
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        byte[] keyBytes = Decoders.BASE64.decode(secret);
-        this.key = Keys.hmacShaKeyFor(keyBytes);
+        this.key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
     }
 
     public String createToken(Authentication authentication) {
@@ -69,11 +59,7 @@ public class TokenProvider implements InitializingBean {
 
     // 토큰으로 클레임을 만들고 이를 이용해 유저 객체를 만들어서 최종적으로 authentication 객체를 리턴
     public Authentication getAuthentication(String token) {
-        Claims claims = Jwts.parserBuilder()
-                .setSigningKey(key)
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
+        Claims claims = getTokenClaims(token);
 
         Collection<? extends GrantedAuthority> authorities = Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
                 .map(SimpleGrantedAuthority::new)
@@ -84,22 +70,23 @@ public class TokenProvider implements InitializingBean {
         return new UsernamePasswordAuthenticationToken(principal, token, authorities);
     }
 
-    // 토큰의 유효성 검증을 수행
-    public boolean validateToken(String token) {
+    // jwt 토큰 리턴
+    private Claims getTokenClaims(String token) {
         try {
-            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
-            return true;
+            return Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
         } catch (SecurityException | MalformedJwtException e) {
-            logger.error("잘못된 JWT 서명입니다.");
+            throw new JwtException("잘못된 JWT 서명입니다.");
         } catch (ExpiredJwtException e) {
-            logger.error("만료된 JWT 토큰입니다.");
+            throw new JwtException("만료된 JWT 토큰입니다.");
         } catch (UnsupportedJwtException e) {
-            logger.error("지원하지않는 JWT 토큰입니다.");
+            throw new JwtException("지원하지않는 JWT 토큰입니다.");
         } catch (IllegalArgumentException e) {
-            logger.error("JWT 토큰이 잘못되없습니다.");
+            throw new JwtException("JWT 토큰이 잘못되없습니다.");
         }
-
-        return false;
     }
 
 }
